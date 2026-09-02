a432e4b18f64c02719ee8af3e3163b7e25df97bb
keith <keithcompere150@gmail.com>
2026-09-02 14:17:39 -0400

    should work

--- schema/config.go
+++ schema/config.go
@@ -1,5 +1,30 @@
 package schema
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type Config struct {
 	TravelDuration string `json:"travel_duration"`
+}
+
+func (c Config) TransformParams() map[string]string {
+	var m map[string]any
+	config := make(map[string]string)
+	bytes, err := json.Marshal(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(bytes, &m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for k, v := range m {
+		val, ok := v.(string)
+		if ok && val != "" {
+			config[k] = val
+		}
+	}
+	return config
 }
